b95e97fb349011c1b6a1a4484f4c639a24996a0e
d0cd <pranavsaig@gmail.com>
2023-02-11 14:35:20 -0800

    Implement AleoV0::json

--- circuit/network/src/v0.rs
+++ circuit/network/src/v0.rs
@@ -48,6 +48,7 @@ use snarkvm_circuit_types::{
 };
 
 use core::fmt;
+use snarkvm_circuit_types::environment::CircuitJSON;
 
 type E = Circuit;
 
@@ -540,6 +541,13 @@ impl Display for AleoV0 {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         // TODO (howardwu): Find a better way to print the circuit.
         fmt::Display::fmt(&Circuit, f)
+    }
+}
+
+impl AleoV0 {
+    /// Returns the JSON representation of the constraint system.
+    pub fn json() -> CircuitJSON {
+        E::json()
     }
 }
 
