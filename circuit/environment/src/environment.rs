b1fba220949529243255545ab938fa280b220fa4
d0cd <pranavsaig@gmail.com>
2023-02-18 09:42:37 -0800

    Remove json method from Environment trait

--- circuit/environment/src/environment.rs
+++ circuit/environment/src/environment.rs
@@ -190,7 +190,4 @@ pub trait Environment:
 
     /// Clears and initializes an empty environment.
     fn reset();
-
-    /// Returns the JSON representation of the constraint system.
-    fn json() -> CircuitJSON;
 }
